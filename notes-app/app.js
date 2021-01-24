a9a0275c7441904095840e0e31067f66c54961c5
AsmaaKhamis <48033443+AsmaaKhamis@users.noreply.github.com>
2021-01-24 09:58:16 +0200

    =init1

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,4 +1,5 @@
 const chalk =require('chalk')
+const { demandOption, string } = require('yargs')
 const yargs = require('yargs')
 const get = require('./notes.js')
 //  const msg =get()
@@ -12,8 +13,22 @@ yargs.version('1.1.0')
 yargs.command({
     command: 'add',
     describe: 'Add a new note',
-    handler: function(){
-        console.log('Adding a new note!')
+    builder:{
+        title:{
+            describe:'Note title',
+            demandOption: true,
+            type:'string'
+        },
+        body:{
+            describe:'Note body',
+            demandOption:true,
+            type:string
+        }
+
+    },
+    handler: function(argv){
+        console.log('Title: '+argv.title)
+        console.log('Body: '+argv.body)
     }
 
 })
@@ -44,14 +59,11 @@ yargs.command({
 })
 
 
-
-
-
-
-
-
 // add, removr , read , list
-console.log(yargs.argv)
+// console.log(yargs.argv)
+// that line doing the same job as the above
+yargs.parse()
+
 
 
 
